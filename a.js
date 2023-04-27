e29eb00dea9f59413a426e7028075cfaa8e1059c
Vuhuu29 <108822704+Vuhuu29@users.noreply.github.com>
2023-04-27 12:38:08 +0700

    Up lại code mẫu

--- a.js
+++
@@ -1,5 +0,0 @@
-fetch("http://localhost:3000/users")
-// We get the API response and receive data in JSON format
-  .then((response) => response.json())
-  .then((data) => console.log(data.a))
-  .catch ((error) => console.error(error))
